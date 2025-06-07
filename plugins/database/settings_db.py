dd7314a9e8563cefb312f238b28508b0f595ea5f
haruto775 <prasadpriyanshu1769@gmail.com>
2025-06-07 18:34:41 +0530

    Update settings_db.py

--- plugins/database/settings_db.py
+++ plugins/database/settings_db.py
@@ -1,19 +1,17 @@
-pip install motor
+from motor.motor_asyncio import AsyncIOMotorClient
 from config import Config
-import motor.motor_asyncio
 
-
-client = motor.motor_asyncio.AsyncIOMotorClient(Config.DB_URL)
+client = AsyncIOMotorClient(Config.DB_URL)
 db = client[Config.DB_NAME]
-settings_col = db["settings"]
+settings_collection = db["UserSettings"]
 
 async def save_dump_channel(user_id: int, channel_id: str):
-    await settings_col.update_one(
-        {"_id": user_id},
+    await settings_collection.update_one(
+        {"user_id": user_id},
         {"$set": {"dump_channel": channel_id}},
         upsert=True
     )
 
 async def get_dump_channel(user_id: int):
-    user_data = await settings_col.find_one({"_id": user_id})
-    return user_data.get("dump_channel") if user_data else None
+    data = await settings_collection.find_one({"user_id": user_id})
+    return data.get("dump_channel") if data else None
