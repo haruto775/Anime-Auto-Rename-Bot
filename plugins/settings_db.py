9bd0b534e2cd03ba097f7521c4b0423c71e8d429
haruto775 <prasadpriyanshu1769@gmail.com>
2025-06-07 16:52:17 +0530

    Update settings_db.py

--- plugins/settings_db.py
+++ plugins/settings_db.py
@@ -1,17 +1,17 @@
-from pymongo import MongoClient
-from config import MONGO_URL
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import DB_URL, DB_NAME
 
-client = MongoClient(MONGO_URL)
-db = client["RenameBot"]
+client = AsyncIOMotorClient(DB_URL)
+db = client[DB_NAME]
 settings_collection = db["UserSettings"]
 
 async def save_dump_channel(user_id: int, channel_id: str):
-    settings_collection.update_one(
+    await settings_collection.update_one(
         {"user_id": user_id},
         {"$set": {"dump_channel": channel_id}},
         upsert=True
     )
 
 async def get_dump_channel(user_id: int):
-    data = settings_collection.find_one({"user_id": user_id})
+    data = await settings_collection.find_one({"user_id": user_id})
     return data.get("dump_channel") if data else None
